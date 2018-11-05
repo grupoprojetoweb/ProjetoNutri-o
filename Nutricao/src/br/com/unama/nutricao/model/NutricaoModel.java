c31342d959216d23f64a9c69e4586b63de57bd8a
26146196 <26146196@UNA007-L201-008.serlab.intra>
2018-11-05 17:23:36 -0300

    model 01

--- Nutricao/src/br/com/unama/nutricao/model/NutricaoModel.java
+++ Nutricao/src/br/com/unama/nutricao/model/NutricaoModel.java
@@ -1,5 +1,14 @@
 package br.com.unama.nutricao.model;
 
 public class NutricaoModel {
+private int idade;
+
+public int getIdade() {
+	return idade;
+}
+
+public void setIdade(int idade) {
+	this.idade = idade;
+}
 
 }
